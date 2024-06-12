55a76c0883cf3687542ce25dc01ee6d95a9c84f9
Luke Edwards <lukeedwards30167116@gmail.com>
2024-06-12 08:08:41 +0100

    no need for base yet

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import Markdown from "vite-plugin-md";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "lukeJEdwards.github.io",
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/], // <--
